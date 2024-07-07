ca341d2c3a97a55b03371409d8f1225f395bca3e
Michael Chien <michael5107414@gmail.com>
2024-07-07 13:04:27 +0800

    fix: adjust config

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,7 +4,6 @@ export default {
   verbose: true,
   testEnvironment: "node",
   roots: ["<rootDir>/__tests__"],
-  collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.{ts,js}"],
   coverageProvider: "v8",
   coverageDirectory: "coverage",
